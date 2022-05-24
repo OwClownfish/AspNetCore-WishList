deee81efb7d68b1463001647622fa01a49f309e4
Hielke Rijpstra <rijpstra@rdw.nl>
2022-05-24 14:25:26 +0200

    zz

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;    
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items;
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
